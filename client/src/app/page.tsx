5d13ecdcc792f2bb939a56d919dbc5f8827b6c33
sachin-iam <sachinguptaiam@gmail.com>
2025-07-31 19:22:44 +0530

    feat:page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import Navbar from './homepage/components/Navbar'
 import Hero from './homepage/components/Home'
-
 const page = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
-    
-    
-    
     </>
   )
 }
