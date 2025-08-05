0977d853314159e1a1e4963a28c10ce5b7b2000b
Priyanshu <priyanshupatel405@gmail.com>
2025-08-05 12:24:36 +0530

    Feat: purchases recuring expense

---
+++ client/src/app/dashboard/purchases/recurring-expenses/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RecurringProfileActions from './components/RecurringProfileActions'
+import RecurringExpensesIntro from './components/RecurringExpensesIntro'
+
+
+const page = () => {
+  return (
+    <>
+    <RecurringProfileActions/>
+    <RecurringExpensesIntro/>
+    </>
+  )
+}
+
+export default page
